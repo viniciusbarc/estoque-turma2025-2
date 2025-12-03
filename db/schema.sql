426a212864b7f099fd3100c243eec40902d827ed
JuhRBaptista <109632124+JuhRBaptista@users.noreply.github.com>
2025-12-02 21:55:33 -0300

    Add ProductOrder table to schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,11 @@ barcode text primary key,
 name text not null,
 quantity_in_stock integer not null,
 order_reference_days integer not null);
+
+create table ProductOrder(
+uuid text primary key,
+product text not null,
+quantity integer not null,
+orderDate text not null,
+
+foreign key (product) references products(barcode));
